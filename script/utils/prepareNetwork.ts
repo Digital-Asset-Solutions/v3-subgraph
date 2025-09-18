e19c2aee772589a2d0168387db280a7f4b80faa3
Anthony <anthony.issa@hotmail.fr>
2025-09-18 18:48:53 +0200

    wip

--- script/utils/prepareNetwork.ts
+++ script/utils/prepareNetwork.ts
@@ -18,6 +18,7 @@ export enum NETWORK {
   WORLDCHAIN = 'worldchain-mainnet',
   ZKSYNC_ERA = 'zksync-era',
   ZORA = 'zora-mainnet',
+  EDEN_TESTNET = 'eden-testnet',
 }
 
 export enum SUBGRAPH_TYPE {
